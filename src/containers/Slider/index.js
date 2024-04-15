c2574aa6f0852191423995ff703922c31120e91b
littlekanine <elia.kopff3891@gmail.com>
2024-04-15 16:05:50 +0200

    fix slide empty problem / button radio follow

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,15 +10,15 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+
   useEffect(() => {
-    nextCard();
-  });
+    const timer = setTimeout(() => {
+      setIndex((prevIndex) => (prevIndex === (byDateDesc?.length ?? 0) - 1 ? 0 : prevIndex + 1));
+    }, 5000);
+
+    return () => clearTimeout(timer); // Fonction de nettoyage du timer 
+  }, [index, byDateDesc?.length]);
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
@@ -45,7 +45,7 @@ const Slider = () => {
                   key={`${event.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
